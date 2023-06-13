ecf684d21d6942882ac39e151a0bcc0029f5b8fe
monukumar98 <monu012as@gmail.com>
2023-06-13 11:42:49 +0530

    Operators

---
+++ Lec1/Assignment_Operators.java
@@ -0,0 +1,15 @@
+package Lec1;
+
+public class Assignment_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 8;
+		x += 3;// x= x+3;
+		System.out.println(x);
+		x-=5;// x= x-5; 
+		System.out.println(x);
+
+	}
+
+}
---
+++ Lec1/Perimenter.java
@@ -0,0 +1,10 @@
+package Lec1;
+
+public class Perimenter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
