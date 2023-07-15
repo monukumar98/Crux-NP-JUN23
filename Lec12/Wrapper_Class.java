2eac42248734b551f95dd0951ca1e9f0203c1057
monukumar98 <monu012as@gmail.com>
2023-07-15 16:48:42 +0530

    loops

---
+++ Lec12/Wrapper_Class.java
@@ -0,0 +1,19 @@
+package Lec12;
+
+public class Wrapper_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 990;// auto-boxing
+		System.out.println(a);
+		int a1 = a;// UN-boxing
+		Integer b1 = -18;
+		Integer b2 = -18;
+		Integer c2 = 181;
+		Integer c1 = 181;
+		System.out.println(b1 == b2);
+		System.out.println(c1 == c2);
+
+	}
+
+}
