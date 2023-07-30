46d8baea9c29abe4d1f1fb42c6384d8cad3a6567
monukumar98 <monu012as@gmail.com>
2023-07-30 21:34:57 +0530

    DNC

---
+++ Lec20/Prime_Check.java
@@ -0,0 +1,24 @@
+package Lec20;
+
+public class Prime_Check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 79;
+		System.out.println(ISprime(n));
+
+	}
+
+	public static boolean ISprime(int n) {
+		int div = 2;
+
+		while (div * div <= n) {
+			if (n % div == 0) {
+				return false;
+			}
+			div++;
+		}
+		return true;
+
+	}
+}
